c407e2ee07731eb9e8d7c2a9be6c48de0ca10cde
Waseem Ahmad Naeem <ahmadwaseem237@gmail.com>
2017-06-22 13:18:48 +0500

    Sign is Set as Staring page
    Logically sign in should be first appearing page of application so Signin Page is set as First Appearing Page.

--- www/js/app.js
+++ www/js/app.js
@@ -77,6 +77,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
   // if none of the above states are matched, use this as the fallback
   //$urlRouterProvider.otherwise('/signin');
-   $urlRouterProvider.otherwise('/app/playlists');
+   $urlRouterProvider.otherwise('/app/SignIn');
   //$urlRouterProvider.otherwise('/playlist');
 });
